36ab9df7af4739e65190f7c16ecfc6f0ad496baa
mountainwen <12750432@gmail.com>
2024-05-29 10:08:15 +0800

    week06

---
+++ week15/week15-1-not finish.cpp
@@ -0,0 +1,7 @@
+int equalSubstring(char* s, char* t, int maxCost) {
+    int N= strlen(s);//先知道字串的長度
+    for(int i=0;i<N;i++){//字串的迴圈
+        printf("%c %c\n",s[i],t[i]);//字串的陣列
+    }
+    return 0;
+}
