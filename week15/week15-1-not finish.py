b40f187b10f78bea18f98d2d4c7e1e6934c9fc97
mountainwen <12750432@gmail.com>
2024-05-29 10:10:13 +0800

    week15

--- week15/week15-1-not finish.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-        #先用python把字串的迴圈寫出來
-        N=len(s)#先知道字串的長度
-        for i in range(N):#字串的迴圈
-            print(s[i],t[i])#字串的陣列
-
-        return 0
